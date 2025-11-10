86dbf8b2115e85795373b312d62d97ac0a56a794
Rihem <Rihem4085@gmail.com>
2025-11-10 14:21:15 +0100

    update the code and add req

--- main.py
+++ main.py
@@ -10,9 +10,32 @@ name=st.text_input("enter you name")
 #if name :
   #  st.success(f"Hi{name} welcome to my app ")
 
+
+  # ------------  KEEP DATA AFTER REFRESH ------------
+if "df" not in st.session_state:
+    st.session_state.df = None
+    #If there is NO variable named "df" saved in session_state, create it and set it to None.
+
 uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"]) 
+
+# if a new file is uploaded ⇒ read & save it
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
+    st.session_state.df = pd.read_csv(uploaded_file)
+# get the saved dataframe
+df = st.session_state.df
+
+
+
+# ------------  MAIN UI ------------
+if df is not None:
+        # ✅ Clear Button here
+    if st.button("Clear Data"):
+        st.session_state.df = None
+        st.rerun()
+
+        
+    st.success("File uploaded successfully!")
+
     if st.checkbox("show data"):
         st.dataframe(df.head()) # Display data
     #st.table(df) # Static table
@@ -33,7 +56,8 @@ if uploaded_file is not None:
     #multi-select box 
     choose_col = st.multiselect("Select columns to show :", df.columns.to_list(),default=df.columns.to_list())
 
-    #display data 
+    #display data     # Display selected rows + columns
+
     st.write(df[:no_rows][choose_col])
 
      
@@ -45,8 +69,9 @@ if uploaded_file is not None:
     st.success("Statistical analysis complete!")
 
 
+    # ---------------- VISUALIZATION ----------------
 
-    st.subheader("viz")
+    st.subheader("Visualization")
 
 
       
